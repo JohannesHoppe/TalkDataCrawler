23a7422a93a998c1fca3581cf61a6c33f9d76059
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-07-15 12:45:26 +0200

    live coding

--- TalkDataCrawler/TalkDataCrawler/app.js
+++ TalkDataCrawler/TalkDataCrawler/app.js
@@ -1,9 +1,19 @@
 ﻿var DwxCrawler = require('./dwxCrawler'),
+    fs = require("fs"),
     dwxCrawler,
-    crawlerConfig = {};
+    crawlerConfig = {
+        host: 'www.developer-week.de',
+        initialPath: '/Programm',
+        discoverRegex: [
+            /(\shref\s?=\s?)['"](\/Programm\/Veranstaltung\/\(event\)\/[^4"']+)/ig
+        ],
+        userAgent: 'DWX 2014 TalkDataCrawler (by Johannes Hoppe)'
+    }
 
 dwxCrawler = new DwxCrawler(crawlerConfig);
 dwxCrawler.on('complete', function(talks) {
 
-    console.log(talks);
+    fs.writeFile("talks.js", JSON.stringify(talks), 'utf8', console.log);
 });
+
+dwxCrawler.start();
--- TalkDataCrawler/TalkDataCrawler/dwxCrawler.js
+++ TalkDataCrawler/TalkDataCrawler/dwxCrawler.js
@@ -1,1 +1,47 @@
-﻿var Simplecrawler = required("simplecrawler");
+﻿var Simplecrawler = require("simplecrawler"),
+    extend = require("extend"),
+    util = require("util"),
+    events = require("events"),
+    cheerio = require("cheerio");
+
+function Crawler(config) {
+
+    this.config = config;
+    this.talks = [];
+
+    this._crawler = new Simplecrawler(this.config.host);
+    extend(this._crawler, config);
+   
+    this._crawler.on("fetchcomplete", this._fetchcomplete.bind(this));
+    
+    this._crawler.on("complete", function() {
+        this.emit("complete", this.talks);
+    }.bind(this)); 
+
+}
+
+util.inherits(Crawler, events.EventEmitter);
+
+Crawler.prototype._fetchcomplete = function(queueItem, responseBuffer) {
+
+    var html = responseBuffer.toString();
+    var $ = cheerio.load(html);
+    var title = $(".container h2").first().text();
+    var description = $(".container P").first().text();
+    
+    if (!title) {
+        return;
+    }
+
+    this.talks.push({
+        title: title,
+        description: description
+    });
+}
+
+Crawler.prototype.start = function () {
+    this._crawler.start();
+}
+
+module.exports = Crawler;
+
